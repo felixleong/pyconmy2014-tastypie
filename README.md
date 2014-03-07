9444769d3971760222bb453ff78510092d4c04c5
Seh Hui, Leong <felixleong@gmail.com>
2014-03-07 15:17:59 +0800

    Initial setup.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Developing Powerful RESTful APIs with Django Tastypie -- Sample Project and Slides
+
+Presented during [Mini-PyCon Malaysia 2014](http://www.pycon.my/mini-pycon-my-2014).
+
+## Setup
---
+++ blog/settings/local.example.py
@@ -0,0 +1,18 @@
+from blog.settings.base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ''
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
