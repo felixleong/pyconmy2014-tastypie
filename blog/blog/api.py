62475b64b8ce610ead31cbf3b3b093aaa12cc569
Seh Hui, Leong <felixleong@gmail.com>
2014-03-10 09:55:37 +0800

    Setting up the API front.

--- blog/blog/api.py
+++ blog/blog/api.py
@@ -1,1 +1,30 @@
+from django.contrib.auth import get_user_model
 from tastypie.resources import ModelResource
+from taggit.models import Tag
+from .models import Article
+from ..api import v1_api
+
+
+# The user resource from Django Auth
+class UserResource(ModelResource):
+    class Meta:
+        queryset = get_user_model().objects.all()
+        resource_name = 'user'
+
+
+# Tag resource for taggit model
+class TagResource(ModelResource):
+    class Meta:
+        queryset = Tag.objects.all()
+        resource_name = 'tag'
+
+
+class ArticleResource(ModelResource):
+    class Meta:
+        queryset = Article.objects.all()
+        resource_name = 'article'
+
+
+v1_api.register(UserResource())
+v1_api.register(TagResource())
+v1_api.register(ArticleResource())
